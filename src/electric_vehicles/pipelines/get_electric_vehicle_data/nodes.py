462a8f338da5a91fbc1407e10e80d94cd7b808cf
Daniel <danielrod147@gmail.com>
2023-08-18 00:13:59 -0300

    pipeline para download de dados

---
+++ src/electric_vehicles/pipelines/get_electric_vehicle_data/nodes.py
@@ -0,0 +1,22 @@
+"""
+Pipeline 'get_electric_vehicle_data'
+generated using Kedro 0.18.12
+"""
+import requests
+
+
+def download_data(
+    url: str,
+    save_path: str
+) -> None:
+    """
+    Função que carrega base de dados de um dado endereço na internet.
+    """
+    data = requests.get(url)
+
+    # Verifica se o download foi bem sucedido
+    if data.status_code == 200:
+        with open(save_path, 'wb') as f:
+            f.write(data.content)
+    else:
+        print('Erro ao carregar dados.')
---
+++ src/electric_vehicles/pipelines/get_electric_vehicle_data/pipeline.py
@@ -0,0 +1,22 @@
+"""
+This is a boilerplate pipeline 'get_electric_vehicle_data'
+generated using Kedro 0.18.12
+"""
+
+from kedro.pipeline import Pipeline, pipeline, node
+from .nodes import download_data
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            node(
+                func=download_data,
+                inputs={
+                    "url": "params:electric_vehicle_data_url",
+                    "save_path": "params:electric_vehicle_data_save_path",
+                    },
+                outputs=None,
+                name="download_data",
+            )
+        ]
+    )
